063dd9065adbe6a4f180de39e6aa1d652449b94a
nicolas <nhomble@terpmail.umd.edu>
2020-12-07 00:18:20 -0500

    - documents how to kill a topology - adds log in bolt

--- src/main/java/io/github/nhomble/storm/temp/TemperatureTopology.java
+++ src/main/java/io/github/nhomble/storm/temp/TemperatureTopology.java
@@ -1,5 +1,6 @@
 package io.github.nhomble.storm.temp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.storm.ILocalCluster;
 import org.apache.storm.kafka.spout.KafkaSpout;
 import org.apache.storm.kafka.spout.KafkaSpoutConfig;
@@ -13,9 +14,11 @@ import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
+import org.slf4j.Logger;
 
 import java.util.Map;
 
+@Slf4j
 public class TemperatureTopology extends ConfigurableTopology {
 
     public static void main(String[] args) {
@@ -49,6 +52,7 @@ public class TemperatureTopology extends ConfigurableTopology {
 
         @Override
         public void execute(Tuple tuple) {
+            log.info("Executing {}", tuple);
             collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
             collector.ack(tuple);
         }
